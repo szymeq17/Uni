219c52ebe74f4bb7745b058743ebfae73946b24d
szymeq17 <rysz.szymon@gmail.com>
2020-11-01 14:09:01 +0100

    lista 4 z1, z2, z3

---
+++ Kurs WWW/Lista 4/zad2script.js
@@ -0,0 +1,33 @@
+
+
+var adverbs = ["bezmyślnie", "arogancko", "chamsko", "cicho", "dostojnie",
+"dosadnie", "fatalnie", "finezyjnie", "energicznie", "marudnie"];
+var verbs = ["poszedł", "złapał", "powiedział", "milczał", "krzyczał",
+"skakał", "leciał", "śpiewał", "pił", "jadł"];
+var adjectives = ["fajną", "spoko", "piękną", "paskudną", "paskudne",
+"śmierdzące", "zepsute", "pyszne", "przerażające", "głupie"];
+var nouns = ["piwo", "wódkę", "dziewczynę", "chłopaka", "prezydenta",
+"koronawirusa", "Polskę", "burgera", "kanapkę", "mleko"];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for(i=0; i<adverbs.length; i++) {
+  $("#words").append("<tr><td>" + adverbs[i] + "</td><td>" + verbs[i] +
+  "</td><td>" + adjectives[i] + "</td><td>" + nouns[i] + "</td></tr>");
+}
+
+$("#gen-btn").click(function() {
+  let w1 = getRandomIntInclusive(0, verbs.length-1);
+  let w2 = getRandomIntInclusive(0, verbs.length-1);
+  let w3 = getRandomIntInclusive(0, verbs.length-1);
+  let w4 = getRandomIntInclusive(0, verbs.length-1);
+  let word = adverbs[w1] + " " + verbs[w2] + " " + adjectives[w3] + " "
+  + nouns[w4] + ".";
+  let first = word.charAt(0).toUpperCase();
+  word = first + word.substring(1);
+  $("#result").html(word)
+})
---
+++ Kurs WWW/Lista 4/zad3script.js
@@ -0,0 +1,49 @@
+function showMarked() {
+  let marked = "Zaznaczone: ";
+  $("img").each(function() {
+    if($(this).data("marked")) {
+      marked += $(this).attr("id") + ", ";
+    }
+  })
+  marked = marked.substring(0, marked.length-2);
+  $("#log").text(marked);
+}
+
+function selectAll() {
+  $("img").each(function() {
+    $("img").data("marked", true);
+    $(this).css("border-color", "#00A1FF");
+  })
+  showMarked();
+}
+
+function unselectAll() {
+  $("img").each(function() {
+    $("img").data("marked", false);
+    $(this).css("border-color", "black");
+  })
+  showMarked();
+}
+
+$(document).ready(function() {
+
+$("img").each(function() {
+  $(this).data("marked", false);
+})
+
+$("img").each(function() {
+  $(this).click(function() {
+    if($(this).data("marked")) {
+      $(this).data("marked", false);
+      $(this).css("border-color", "black");
+    }
+    else {
+      $(this).data("marked", true);
+      $(this).css("border-color", "#00A1FF");
+    }
+    showMarked();
+  });
+})
+$("#select-all").click(selectAll);
+$("#unselect-all").click(unselectAll);
+})
